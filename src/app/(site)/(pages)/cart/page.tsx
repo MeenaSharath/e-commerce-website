977a9c4bf6ed1c88d500cfde6138fb6cd0d694e1
MeenaSharath <meenanairkcy131@gmail.com>
2025-06-13 21:56:53 +0530

    updated header

--- src/app/(site)/(pages)/cart/page.tsx
+++ src/app/(site)/(pages)/cart/page.tsx
@@ -1,25 +1,18 @@
+"use client";
+
 import React from "react";
 import Cart from "@/components/Cart";
 import { Provider } from "react-redux";
 import { store, persistor } from "@/redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "Cart Page | NextCommerce Nextjs E-commerce template",
-  description: "This is Cart Page for NextCommerce Template",
-  // other metadata
-};
-
 const CartPage = () => {
   return (
-    
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <Cart />
+        <Cart />
       </PersistGate>
     </Provider>
-    
   );
 };
 
