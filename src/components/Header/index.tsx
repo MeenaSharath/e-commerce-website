910697cc7dfbef947d5205dba44d2417c1b67859
MeenaSharath <meenanairkcy131@gmail.com>
2025-06-13 20:57:43 +0530

    updated header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -280,7 +280,7 @@ const Header = () => {
 )}
 
    {isAuthenticated && !hideSignOut && !hideSignin &&(            
-  <Link href={accountLink} className="flex items-center gap-2 hover:text-primary">
+  <Link href="/my-account" className="flex items-center gap-2 hover:text-primary">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className="w-8 h-8 text-blue"
