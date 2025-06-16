6e152f77c992622af5239fc6ba0c5299945ed93c
MeenaSharath <meenanairkcy131@gmail.com>
2025-06-16 15:17:20 +0530

    updated for first page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,14 +6,14 @@ import BestSeller from "./BestSeller";
 import CounDown from "./Countdown";
 import Testimonials from "./Testimonials";
 import Newsletter from "../Common/Newsletter";
-import { useAuthRedirect } from '@/hooks/useAuthRedirect';
+// import { useAuthRedirect } from '@/hooks/useAuthRedirect';
 
 const Home = () => {
-  const checkingAuth = useAuthRedirect();
+  // const checkingAuth = useAuthRedirect();
 
-  if (checkingAuth) {
-    return <div className="text-center mt-20 text-lg">Checking authentication...</div>;
-  }
+  // if (checkingAuth) {
+  //   return <div className="text-center mt-20 text-lg">Checking authentication...</div>;
+  // }
   return (
     
     <main>
