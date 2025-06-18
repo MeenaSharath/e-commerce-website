88c12c1f91c7a3261eed02639b8537b4c11980fc
MeenaSharath <meenanairkcy131@gmail.com>
2025-06-18 22:43:10 +0530

    homepage changes

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,20 +1,6 @@
 'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation'; 
-import { useAppSelector } from '@/redux/store';
 import Home from '@/components/Home';
 
 export default function HomePage() {
-  const router = useRouter();
-  const isAuthenticated = useAppSelector(state => state.authReducer.isAuthenticated);
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      router.replace('/signin');
-    }
-  }, [isAuthenticated]);
-
-  if (!isAuthenticated) return null;
-
   return <Home />;
 }
